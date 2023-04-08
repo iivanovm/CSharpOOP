00acc0b50653b7d0403a8e8777ed44736e9a24a1
Ivan Ivanov <ivan_marinov_ivanov@yahoo.com>
2023-04-08 22:50:34 +0300

    Add MyExam - 08/04/2023 - Field {I faild this exam}

---
+++ TryExam/RobotService/Models/Models/Robot/IndustrialAssistant.cs
@@ -0,0 +1,9 @@
+﻿namespace RobotService.Models.Models.Robot;
+
+public class IndustrialAssistant : Robot
+{
+    public IndustrialAssistant(string model) 
+        : base(model, 40000, 5000)
+    {
+    }
+}
